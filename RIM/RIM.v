b03bbe13e76f90a5d5a1924cbcfda08ede8edf55
ZiRu11102165 <124028666+ZiRu11102165@users.noreply.github.com>
2025-02-06 01:54:53 +0800

    v20250205

--- RIM/RIM.v
+++ RIM/RIM.v
@@ -1,25 +1,5 @@
 `timescale 1ns / 1ps
 
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date: 2024/12/23 14:11:51
-// Design Name: Rat in a Maze
-// Module Name: RIM
-// Project Name:
-// Target Devices:
-// Tool Versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-///////////////////////////////////////////////////////////////////////////////////
-
 module RIM(
     output reg out_valid,
     output reg [2:0]out_row,
@@ -29,25 +9,12 @@ module RIM(
     input rst_n,
     input in_valid
 );
-    reg [7:0] store_maze [0:7];
-    integer in_count=0;
-    integer i, j;
-    reg maze_done_flag;
-
-    /* 輸入 8 clock 的 maze 使迷宮變成 8x8 */
-    always@ (posedge clk or negedge rst_n) begin 
-        if (in_valid & (in_count<8)) begin
-            store_maze[in_count] <= maze;
-            in_count <= in_count + 1;
-        end
-    end
-    always @(*) begin
-        maze_done_flag <= (store_maze[7] > 0 )? 1:0;
-    end
     /* 建立FSM */
     parameter[1:0] IDLE = 0, SEARCH = 1, DONE = 2;
+    integer in_count, out_count, i;
+    reg [7:0] store_maze [0:7];
     reg[1:0]Q, Q_NEXT;
-    reg [2:0] now_row, now_col, store_row, store_col, next_row, next_col;
+    reg [2:0] now_row, now_col;
     reg [2:0] store_fold_row [0:8];
     reg [2:0] store_fold_col [0:8];
     reg [2:0] store_ans_row [0:14];
@@ -66,87 +33,108 @@ module RIM(
     always@(*)begin
         case(Q)
             IDLE:
-                if(maze_done_flag & !in_valid) Q_NEXT = SEARCH;
+                if(!in_valid & in_count>7) Q_NEXT = SEARCH;
                 else Q_NEXT = IDLE;
             SEARCH:
                 if (!(now_row==7 & now_col==0)) Q_NEXT = SEARCH;
                 else Q_NEXT = DONE;
             DONE:
-                Q_NEXT = DONE;
+                if (out_count<14) Q_NEXT = DONE;
+                else Q_NEXT = IDLE;
             default:
                 Q_NEXT = IDLE;
         endcase
     end
     always@(posedge clk or negedge rst_n)begin
-        case(Q)
-            IDLE: begin 
-                back_flag <= 0;
-                now_col <= 7;
-                now_row <= 0;
-                count_fold <= 0;
-                count_ans <= 0;
-            end
-            SEARCH: begin
-                $display("目前位置[%d][%d] ", now_row, now_col);
-                store_ans_row[count_ans] <= now_row;
-                store_ans_col[count_ans] <= now_col;
-                $display("count_ans = %d", count_ans);
-                if (!back_flag) begin
-                    if ((now_row + 1 < 8) & (store_maze[now_row + 1][now_col])) begin 
-                        // $display(" 勞鼠往下走了! 1");
-                        now_row <= now_row + 1;
-                    end
-                    else if ((now_col - 1 >= 0) & (store_maze[now_row][now_col - 1])) begin
-                        // $display(" 勞鼠往右走了! 2");
-                        now_col <= now_col - 1;
-                    end
-                    else begin
-                        // $display(" 勞鼠沒路走了! 3");
-                        back_flag <= 1;
-                        if (count_fold > 0)begin
-                            now_row <= store_fold_row[count_fold - 1];
-                            now_col <= store_fold_col[count_fold - 1];
-                            count_fold <= count_fold - 1;
-                        end
-                        count_ans <= count_ans - (now_row - store_fold_row[count_fold - 1]) - (store_fold_col[count_fold - 1] - now_col);
-                        $display("count_ans = %d - (%d -%d) - (%d -%d)", count_ans, now_row, store_fold_row[count_fold - 1], store_fold_col[count_fold - 1], now_col);
-                    end
-                    if (now_row<8 & now_col>0 & store_maze[now_row+1][now_col] & store_maze[now_row][now_col-1])begin
-                        // $display(" 勞鼠走到岔路了! 岔路位置[%d][%d] ", now_row, now_col);
-                        store_fold_row[count_fold] <= now_row;
-                        store_fold_col[count_fold] <= now_col;
-                        count_fold <= count_fold + 1;
-                    end
-                end
-                else begin
-                    if ((now_col - 1 >= 0) & (store_maze[now_row][now_col - 1])) begin
-                        // $display(" 勞鼠往右走了! 4");
-                        now_col <= now_col - 1;
-                        back_flag <= 0;
-                    end
-                    
-                end
-                count_ans <= count_ans + 1;
-            end
-
-            DONE: begin
-                if (!store_maze[0][7]) begin
-                    $display(" 左上角為0! ");
-                    out_valid <= 0;
+        if (!rst_n) begin
+            in_count <= 0;
+            out_row <= 0;
+            out_col <= 0;
+            out_count <= 0;
+            back_flag <= 0;
+            now_col <= 7;
+            now_row <= 0;
+            out_valid <= 0;
+            count_fold <= 0;
+            count_ans <= 0;
+        end
+        else begin
+            case(Q)
+                IDLE: begin 
+                    in_count <= 0;
                     out_row <= 0;
                     out_col <= 0;
-                end
-                else begin 
-                    $display(" 走到終點! ");
-                    out_valid <= 1;
-                    for (i = 0; i <= 15; i = i+1) begin
-                        $display("store_ans_row[%d] ", store_ans_row[i]);
-                        $display("store_ans_col[%d] ", store_ans_col[i]);
-                        out_row <= store_ans_row[i];
-                        out_col <= store_ans_col[i];
+                    out_count <= 0;
+                    back_flag <= 0;
+                    now_col <= 7;
+                    now_row <= 0;
+                    out_valid <= 0;
+                    count_fold <= 0;
+                    count_ans <= 0;
+                    // for (i = 0; i < 15; i = i + 1) begin
+                    //     store_ans_row[i] <= 3'b000; // 設為 0
+                    //     store_ans_col[i] <= 3'b000;
+                    // end
+                    // for (i = 0; i < 8; i = i + 1) begin
+                    //     store_maze[i] <= 8'b0000_0000; // 設為 0
+                    // end
+                    
+                    /* 輸入 8 clock 的 maze 使迷宮變成 8x8 */
+                    if (!rst_n) begin
+                        in_count <= 0;
+                    end
+                    else begin
+                        if (in_valid)begin
+                            store_maze[in_count] <= maze;
+                            in_count <= in_count + 1;
+                        end
+                        else in_count <= 0;
                     end
                 end
-            end
-        endcase
+                SEARCH: begin
+                    store_ans_row[count_ans] <= now_row;
+                    store_ans_col[count_ans] <= now_col;
+                    if (!back_flag) begin
+                        if ((now_row + 1 < 8) & (store_maze[now_row + 1][now_col])) begin 
+                            now_row <= now_row + 1;
+                        end
+                        else if ((now_col - 1 >= 0) & (store_maze[now_row][now_col - 1])) begin
+                            now_col <= now_col - 1;
+                        end
+                        else begin
+                            back_flag <= 1;
+                            if (count_fold > 0)begin
+                                now_row <= store_fold_row[count_fold - 1];
+                                now_col <= store_fold_col[count_fold - 1];
+                                count_fold <= count_fold - 1;
+                            end
+                        end
+                        if (now_row<8 & now_col>0 & store_maze[now_row+1][now_col] & store_maze[now_row][now_col-1])begin
+                            store_fold_row[count_fold] <= now_row;
+                            store_fold_col[count_fold] <= now_col;
+                            count_fold <= count_fold + 1;
+                        end
+                    end
+                    else begin
+                        if ((now_col - 1 >= 0) & (store_maze[now_row][now_col - 1])) begin
+                            now_col <= now_col - 1;
+                            back_flag <= 0;
+                        end
+                        
+                    end
+                    if (back_flag) begin
+                        count_ans <= count_ans - (store_ans_row[count_ans - 1] - now_row) - (now_col - store_ans_col[count_ans - 1]);
+                    end
+                    else count_ans <= count_ans + 1;
+                end
+
+                DONE: begin
+                    out_valid <= 1;
+                    out_row <= store_ans_row[out_count];
+                    out_col <= store_ans_col[out_count];
+                    out_count <= out_count + 1; 
+                end
+            endcase
+        end
     end
 endmodule
--- RIM/RIM_tb.v
+++ RIM/RIM_tb.v
@@ -9,19 +9,18 @@ parameter PERIOD  = 10;
 // RIM Inputs
 reg   [7:0]  maze                          = 0 ;
 reg   clk                                  = 0 ;
-reg   rst_n                                = 0 ;
+reg   rst_n                                = 1 ;
 reg   in_valid                             = 0 ;
 
 // RIM Outputs
 wire  out_valid                            ;
 wire  [2:0]  out_row                       ;
 wire  [2:0]  out_col                       ;
+reg [153:0] pattern [1:100];
+reg [153:0] p;
+reg [2:0] gt_row, gt_col, ans_row, ans_col; 
+integer i, j, error, single_error, count, start_, end_;
 
-
-initial
-begin
-    forever #(PERIOD/2)  clk=~clk;
-end
 
 RIM  u_RIM (
     .maze                    ( maze       [7:0] ),
@@ -36,18 +35,51 @@ RIM  u_RIM (
 
 initial
 begin
-    #10;
-    rst_n  =  1;
-    #10;
-    maze = 8'b10000000; in_valid=1; #10; in_valid=0; #10;
-    maze = 8'b11110000; in_valid=1; #10; in_valid=0; #10;
-    maze = 8'b01011111; in_valid=1; #10; in_valid=0; #10;
-    maze = 8'b01011111; in_valid=1; #10; in_valid=0; #10;
-    maze = 8'b01001111; in_valid=1; #10; in_valid=0; #10;
-    maze = 8'b01110111; in_valid=1; #10; in_valid=0; #10;
-    maze = 8'b01000011; in_valid=1; #10; in_valid=0; #10;
-    maze = 8'b01110011; in_valid=1; #10; in_valid=0; #20;
-    #270
+    clk = 0;
+    #10 rst_n = 0;
+    #10 rst_n = 1;
+    error = 0;
+    single_error = 0;
+    in_valid = 0;
+    forever #(PERIOD/2)  clk=~clk;
+end
+
+always @(negedge clk) begin    
+    if (!rst_n) begin
+        count = 0;
+        gt_row = 0;
+        gt_col = 0;
+    end
+    else begin
+        $readmemb("C:\\Users\\USER\\Desktop\\verilog_Exercise\\RIM\\Pattern.dat", pattern);
+        #10
+        for (i=1; i<100; i = i+1) begin
+            p = pattern[i];
+            @(negedge clk);
+            in_valid = 1;
+            maze = p[153:146];
+            @(negedge clk);
+            maze = p[145:138];
+            @(negedge clk);
+            maze = p[137:130];
+            @(negedge clk);
+            maze = p[129:122];
+            @(negedge clk);
+            maze = p[121:114];
+            @(negedge clk);
+            maze = p[113:106];
+            @(negedge clk);
+            maze = p[105:98];
+            @(negedge clk);
+            maze = p[97:90];
+            @(negedge clk);
+            in_valid = 0;
+            maze = 8'bx;
+            #370;
+            @(negedge clk);
+            @(negedge clk);
+        end
+    end
     $finish;
 end
 
